198f44ecec2c859b2f81a08d4bd20723e40a0001
mmingyeomm <aliveck0208@gmail.com>
2024-05-26 20:37:18 +0900

    crud

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -7,9 +9,22 @@ export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
 
-    @Get()
+    @Get('/')
     getAllBoard() {
-        return this.boardsService.getAllBoards()
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post('/')
+    createBoard(@Body() createBoardDTO: CreateBoardDto ) : Board {
+
+        return this.boardsService.createBoard(createBoardDTO);
+    }
+
+    //@param = ?id=1234
+    @Get('/:id')
+    getBoardById(@Param('id') id :string): Board{
+        return this.boardsService.getBoardById(id)
 
     }
+
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import {v1 as uuid} from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
 
 @Injectable()
 export class BoardsService {
     private boards: Board[] = []; 
 
-    getAllBoards(){
+    getAllBoards(): Board[]{
         return this.boards
+    }   
+
+    createBoard(createBoardDto : CreateBoardDto){
+        const {title, description} = createBoardDto; 
+
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status:BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board; 
+    }
+
+    getBoardById(id: string){
+        return this.boards.find((board) => board.id === id);
     }
 
 }
